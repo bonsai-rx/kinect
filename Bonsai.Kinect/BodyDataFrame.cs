b9a0264ae57b24a30e46d1166bc6f0e608d8e60d
glopes <glopes@localhost>
2016-02-17 23:02:19 +0000

    Added support for body and body index frame sources.

---
+++ Bonsai.Kinect/BodyDataFrame.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    public class BodyDataFrame
+    {
+        readonly IEnumerable<Body> bodies;
+        readonly Vector4 floorClipPlane;
+        readonly TimeSpan relativeTime;
+
+        public BodyDataFrame(IEnumerable<Body> bodies, Vector4 floorClipPlane, TimeSpan relativeTime)
+        {
+            if (bodies == null)
+            {
+                throw new ArgumentNullException("bodies");
+            }
+
+            this.bodies = bodies;
+            this.floorClipPlane = floorClipPlane;
+            this.relativeTime = relativeTime;
+        }
+
+        public IEnumerable<Body> Bodies
+        {
+            get { return bodies; }
+        }
+
+        public Vector4 FloorClipPlane
+        {
+            get { return floorClipPlane; }
+        }
+
+        public TimeSpan RelativeTime
+        {
+            get { return relativeTime; }
+        }
+    }
+}
---
+++ Bonsai.Kinect/BodyFrameSource.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Kinect;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    public class BodyFrameSource : Source<BodyDataFrame>
+    {
+        public override IObservable<BodyDataFrame> Generate()
+        {
+            return KinectManager.DefaultSensor.SelectMany(kinect =>
+            {
+                return Observable.Using(
+                    () => kinect.BodyFrameSource.OpenReader(),
+                    reader => Observable.FromEventPattern<BodyFrameArrivedEventArgs>(
+                        handler => reader.FrameArrived += handler,
+                        handler => reader.FrameArrived -= handler)
+                        .Select(evt =>
+                        {
+                            using (var frame = evt.EventArgs.FrameReference.AcquireFrame())
+                            {
+                                var bodies = new Body[frame.BodyCount];
+                                frame.GetAndRefreshBodyData(bodies);
+                                return new BodyDataFrame(bodies, frame.FloorClipPlane, frame.RelativeTime);
+                            }
+                        }));
+            });
+        }
+    }
+}
