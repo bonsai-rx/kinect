d1300c3416f8bafa47657dc27ca11386e36d42a0
glopes <glopesdev@users.noreply.github.com>
2016-02-17 17:02:11 +0000

    Added first draft of Kinect depth frame source.

---
+++ Bonsai.Kinect/DepthFrameSource.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Kinect;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    public class DepthFrameSource : Source<IplImage>
+    {
+        public override IObservable<IplImage> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var kinect = KinectSensor.GetDefault();
+                kinect.Open();
+                var reader = kinect.DepthFrameSource.OpenReader();
+                return Observable.FromEventPattern<DepthFrameArrivedEventArgs>(
+                    handler => reader.FrameArrived += handler,
+                    handler => reader.FrameArrived -= handler)
+                    .Select(evt =>
+                    {
+                        var frame = evt.EventArgs.FrameReference.AcquireFrame();
+                        var description = frame.FrameDescription;
+                        var depth = ImageHelper.GetImageDepth(description);
+                        var output = new IplImage(new Size(description.Width, description.Height), depth, 1);
+                        frame.CopyFrameDataToIntPtr(output.ImageData, (uint)(output.WidthStep * output.Height));
+                        return output;
+                    }).Finally(() =>
+                    {
+                        reader.Dispose();
+                        kinect.Close();
+                    });
+            });
+        }
+    }
+}
---
+++ Bonsai.Kinect/ImageHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Kinect;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    class ImageHelper
+    {
+        internal static IplDepth GetImageDepth(FrameDescription description)
+        {
+            switch (description.BytesPerPixel)
+            {
+                case 1: return IplDepth.U8;
+                case 2: return IplDepth.U16;
+                default: throw new InvalidOperationException("Invalid image depth.");
+            }
+        }
+    }
+}
