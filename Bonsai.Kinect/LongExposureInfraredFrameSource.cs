d884d44f5fa5f11e321859f9d294543291108a5a
glopes <glopes@localhost>
2016-02-17 23:09:35 +0000

    Added support for infrared and long exposure infrared frame sources.

---
+++ Bonsai.Kinect/LongExposureInfraredFrameSource.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Kinect;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    public class LongExposureInfraredFrameSource : Source<TimeInterval<IplImage>>
+    {
+        public override IObservable<TimeInterval<IplImage>> Generate()
+        {
+            return KinectManager.DefaultSensor.SelectMany(kinect =>
+            {
+                return Observable.Using(
+                    () => kinect.LongExposureInfraredFrameSource.OpenReader(),
+                    reader => Observable.FromEventPattern<LongExposureInfraredFrameArrivedEventArgs>(
+                        handler => reader.FrameArrived += handler,
+                        handler => reader.FrameArrived -= handler)
+                        .Select(evt =>
+                        {
+                            using (var frame = evt.EventArgs.FrameReference.AcquireFrame())
+                            {
+                                var description = frame.FrameDescription;
+                                var depth = ImageHelper.GetImageDepth(description);
+                                var output = new IplImage(new Size(description.Width, description.Height), depth, 1);
+                                frame.CopyFrameDataToIntPtr(output.ImageData, (uint)(output.WidthStep * output.Height));
+                                return new TimeInterval<IplImage>(output, frame.RelativeTime);
+                            }
+                        }));
+            });
+        }
+    }
+}
