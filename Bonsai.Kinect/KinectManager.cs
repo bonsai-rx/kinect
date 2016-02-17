f8a9876785de6b3b59ec834fb29217c2c7846528
glopes <glopes@localhost>
2016-02-17 18:15:51 +0000

    Extended depth frame source to allow subscription sharing.

---
+++ Bonsai.Kinect/KinectManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    static class KinectManager
+    {
+        internal static readonly IObservable<KinectSensor> DefaultSensor = CreateSensor();
+
+        static IObservable<KinectSensor> CreateSensor()
+        {
+            return Observable.Defer(() =>
+            {
+                var kinect = KinectSensor.GetDefault();
+                kinect.Open();
+                return Observable.Return(kinect)
+                                 .Concat(Observable.Never(kinect))
+                                 .Finally(kinect.Close);
+            })
+            .Multicast(() => new ReplaySubject<KinectSensor>(1))
+            .RefCount();
+        }
+    }
+}
