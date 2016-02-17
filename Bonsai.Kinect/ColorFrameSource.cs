b131241183a96908ce2fea3d37abd45063d9e3e3
glopes <glopesdev@users.noreply.github.com>
2016-02-17 18:42:46 +0000

    Added first draft of color frame source.

---
+++ Bonsai.Kinect/ColorFrameSource.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Kinect;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Kinect
+{
+    public class ColorFrameSource : Source<TimeInterval<IplImage>>
+    {
+        public ColorFrameSource()
+        {
+            ColorConversion = ColorImageFormat.Bgra;
+        }
+
+        public ColorImageFormat ColorConversion { get; set; }
+
+        public override IObservable<TimeInterval<IplImage>> Generate()
+        {
+            return KinectManager.DefaultSensor.SelectMany(kinect =>
+            {
+                return Observable.Using(
+                    () => kinect.ColorFrameSource.OpenReader(),
+                    reader => Observable.FromEventPattern<ColorFrameArrivedEventArgs>(
+                        handler => reader.FrameArrived += handler,
+                        handler => reader.FrameArrived -= handler)
+                        .Select(evt =>
+                        {
+                            using (var frame = evt.EventArgs.FrameReference.AcquireFrame())
+                            {
+                                var description = frame.FrameDescription;
+                                var depth = ImageHelper.GetImageDepth(ColorConversion);
+                                var channels = ImageHelper.GetImageChannels(ColorConversion);
+                                var output = new IplImage(new Size(description.Width, description.Height), depth, channels);
+                                if (ColorConversion == ColorImageFormat.None)
+                                {
+                                    frame.CopyRawFrameDataToIntPtr(output.ImageData, (uint)(output.WidthStep * output.Height));
+                                }
+                                else
+                                {
+                                    frame.CopyConvertedFrameDataToIntPtr(
+                                       output.ImageData,
+                                       (uint)(output.WidthStep * output.Height),
+                                       ColorConversion);
+                                }
+                                return new TimeInterval<IplImage>(output, frame.RelativeTime);
+                            }
+                        }));
+            });
+        }
+    }
+}
--- Bonsai.Kinect/ImageHelper.cs
+++ Bonsai.Kinect/ImageHelper.cs
@@ -19,5 +19,35 @@ namespace Bonsai.Kinect
                 default: throw new InvalidOperationException("Invalid image depth.");
             }
         }
+
+        internal static IplDepth GetImageDepth(ColorImageFormat format)
+        {
+            switch (format)
+            {
+                case ColorImageFormat.Bayer: return IplDepth.U8;
+                case ColorImageFormat.Bgra: return IplDepth.U8;
+                case ColorImageFormat.Rgba: return IplDepth.U8;
+                case ColorImageFormat.Yuv: return IplDepth.U16;
+                case ColorImageFormat.Yuy2: return IplDepth.U16;
+                case ColorImageFormat.None:
+                default:
+                    return IplDepth.U16;
+            }
+        }
+
+        internal static int GetImageChannels(ColorImageFormat format)
+        {
+            switch (format)
+            {
+                case ColorImageFormat.Bayer: return 1;
+                case ColorImageFormat.Bgra: return 4;
+                case ColorImageFormat.Rgba: return 4;
+                case ColorImageFormat.Yuv: return 1;
+                case ColorImageFormat.Yuy2: return 1;
+                case ColorImageFormat.None:
+                default:
+                    return 1;
+            }
+        }
     }
 }
