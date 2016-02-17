35b5a79cfa7f8b799b22f87d859b5f54b5d4a02e
glopes <glopesdev@users.noreply.github.com>
2016-02-17 18:15:51 +0000

    Extended depth frame source to allow subscription sharing.

--- Bonsai.Kinect/DepthFrameSource.cs
+++ Bonsai.Kinect/DepthFrameSource.cs
@@ -3,37 +3,35 @@ using OpenCV.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bonsai.Kinect
 {
-    public class DepthFrameSource : Source<IplImage>
+    public class DepthFrameSource : Source<TimeInterval<IplImage>>
     {
-        public override IObservable<IplImage> Generate()
+        public override IObservable<TimeInterval<IplImage>> Generate()
         {
-            return Observable.Defer(() =>
+            return KinectManager.DefaultSensor.SelectMany(kinect =>
             {
-                var kinect = KinectSensor.GetDefault();
-                kinect.Open();
-                var reader = kinect.DepthFrameSource.OpenReader();
-                return Observable.FromEventPattern<DepthFrameArrivedEventArgs>(
-                    handler => reader.FrameArrived += handler,
-                    handler => reader.FrameArrived -= handler)
-                    .Select(evt =>
-                    {
-                        var frame = evt.EventArgs.FrameReference.AcquireFrame();
-                        var description = frame.FrameDescription;
-                        var depth = ImageHelper.GetImageDepth(description);
-                        var output = new IplImage(new Size(description.Width, description.Height), depth, 1);
-                        frame.CopyFrameDataToIntPtr(output.ImageData, (uint)(output.WidthStep * output.Height));
-                        return output;
-                    }).Finally(() =>
-                    {
-                        reader.Dispose();
-                        kinect.Close();
-                    });
+                return Observable.Using(
+                    () => kinect.DepthFrameSource.OpenReader(),
+                    reader => Observable.FromEventPattern<DepthFrameArrivedEventArgs>(
+                        handler => reader.FrameArrived += handler,
+                        handler => reader.FrameArrived -= handler)
+                        .Select(evt =>
+                        {
+                            using (var frame = evt.EventArgs.FrameReference.AcquireFrame())
+                            {
+                                var description = frame.FrameDescription;
+                                var depth = ImageHelper.GetImageDepth(description);
+                                var output = new IplImage(new Size(description.Width, description.Height), depth, 1);
+                                frame.CopyFrameDataToIntPtr(output.ImageData, (uint)(output.WidthStep * output.Height));
+                                return new TimeInterval<IplImage>(output, frame.RelativeTime);
+                            }
+                        }));
             });
         }
     }
